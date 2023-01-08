68ad571b499d5c06e899acc1975bde99a7ad4eee
Sharrelb <sharrel.browne@outlook.com>
2023-01-08 17:50:17 +0000

    test 3 (red)

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -1,16 +1,21 @@
 import { fizzBuzz } from "./fizzbuzz";
 
 describe('The fizzBuzz function should', () => {
+   
     it ('return 1 when given 1', () => {
-        const actual = fizzBuzz(1);
-    
-    expect(actual).toEqual(1);
-    });
 
-    it ('return 1 when given ', () => {
-        const actual = fizzBuzz(3);
+        expect(fizzBuzz(1)).toEqual(1);
+        });
+        
+
+    it ('return 1 when given 3', () => {
+
+    expect(fizzBuzz(3)).toEqual('Fizz');
+    });
     
-    expect(actual).toEqual('Fizz');
+    it ('return 1 when given 6 ', () => {
+    
+    expect(fizzBuzz(6)).toEqual('Fizz');
     });
 
 
